7a8951eee8694f1f95d0901b3ae23368ed2ffa58
Dholon AKter <dholonakter721@gmail.com>
2025-02-03 15:51:05 +0100

    understanding OOP and SOLID principles

--- SOLID-PRINCIPLES/SOLID-PRINCIPLES/Program.cs
+++ SOLID-PRINCIPLES/SOLID-PRINCIPLES/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace SOLID_PRINCIPLES;
+﻿using SOLID_PRINCIPLES.DRP_Principles;
+
+namespace SOLID_PRINCIPLES;
 
 public class Program
 {
 	static void Main(string[] args)
 	{
+		// Vehicle
+		VehicleService vs = new VehicleService();
+		Vehicle vehicle = new Truck();
+		Vehicle vehicle2 = new Auto();
+
+		vs.CreateNewVehicle(vehicle);
+		vs.CreateNewVehicle(vehicle2);
+		
+		Cat cat = new Cat();
+		cat.MakeSound();
+	
+
 		Idatabase idatabase = new MySQLDatabase();  //using Polymorphism
 
 		Idatabase idatabase1 = new PostgreSqlDatabase();
@@ -12,6 +26,7 @@ public class Program
 		employeeService.SaveEmployee("Employee data saving to the MySQLDatabase");
 		EmployeeService employeeService1 = new EmployeeService(idatabase1);
 		employeeService1.SaveEmployee("Employee data saving  to the PostgreSQL databas");
+
 
 	}
 }
