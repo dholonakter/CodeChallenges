ac6b804638bb1d45ad1c53156268174b152c6c66
Dholon AKter <dholonakter721@gmail.com>
2025-04-19 20:17:43 +0200

    codewar and leetcode practice added

---
+++ CodeWars/CodePractice/LeetAndWarTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using LeetcodeInterview;
+
+namespace LeetAndWarTest;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Leetcode leetcode = new Leetcode();
+        Console.WriteLine(leetcode.BooleanToString(false));
+    }
+}
---
+++ CodeWars/CodePractice/LeetcodeInterview/Leetcode.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetcodeInterview
+{
+	public class Leetcode
+	{
+		//Leetcode 88. Merge Sorted Array- could not solve it yet
+		public void Merge(int[] nums1, int m, int[] nums2, int n)
+		{
+			m=nums1.Length;
+			n=nums2.Length;
+			for (int i = 0; i < m; i++)
+			{
+				for (int j = 0; j < n; j++)
+				{
+					m= nums1[i] + nums2[j];
+				}
+			}
+			n=nums2.Length;
+			
+
+			for(int i = 0;	i < m+n; i++) 
+			{
+				//int result=
+			}
+		}
+		//Leetcode 58. Length of Last Word
+		public int LengthOfLastWord(string s)
+		{
+			int result = s.Trim().Split(' ').Last().Length;
+			return result;
+		}
+
+	}
+}
